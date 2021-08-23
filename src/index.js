bd887880754158d20e1f8db26247d7315dbbc5ad
Tendai <tendai1@hotmail.com>
2021-08-23 23:39:01 +0200

    Add the delete

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom"
 //component file
 import TodoContainer from "./components/TodoContainer"
+
+<React.StrictMode>
+  <TodoContainer />
+</React.StrictMode>
 ReactDOM.render(<TodoContainer />, document.getElementById("root"))
