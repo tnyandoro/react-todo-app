dba642b319426ea3b9092a5e00211dec0efb8e96
Tendai <tendai1@hotmail.com>
2021-08-23 21:51:05 +0200

    create an item todo

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Welcome to the todo item</h3>
+      </div>
+    )
+  }
+}
