6cd4575d867903e6e1ba519f973f592c9f1b51c0
Tendai <tendai1@hotmail.com>
2021-08-24 13:08:40 +0200

    Add functional components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
 //component file
-import TodoContainer from "./components/TodoContainer"
-import "./App.css"
+// import TodoContainer from "./components/TodoContainer"
+// import "./App.css"
+import TodoContainer from "./functionBased/components/TodoContainer"
+//stylesheet
+import "./functionBased/App.css"
 
 <React.StrictMode>
   <TodoContainer />
